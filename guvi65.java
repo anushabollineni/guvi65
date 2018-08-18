7c8dc285875501eda1c4368b5e2de76604d33201
anushabollineni <41730220+anushabollineni@users.noreply.github.com>
2018-08-18 10:43:36 +0530

    Create guvi65.java

---
+++ guvi65.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		 int i = 0;
+        if(System.out.printf("  ") == null) {}
+        for(i = 1; i < 2; System.out.println("Hello World."))
+        {
+            i++;
+     	}
+     }
+	}
